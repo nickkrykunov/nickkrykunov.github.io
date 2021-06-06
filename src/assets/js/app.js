5daedb0e9a5c5cf65d1d0a158642e688d4cba1bb
Nickolay Krykunov <nik.krikunov.99@gmail.com>
2021-06-06 15:23:08 +0300

    add mobile breakpoint for intro & menu & advantages

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,1 +1,10 @@
-console.log("Hello!");
+const body = document.querySelector('body');
+const header_burger = document.querySelector('.header__burger');
+const header_menu = document.querySelector('.header__menu');
+
+
+header_burger.addEventListener('click', function(event){
+    header_burger.classList.toggle('active');
+    header_menu.classList.toggle('active');
+    body.classList.toggle('no-scroll');
+});
