d137c09708b4b8366aad9597f5fecc35844f4b12
Nickolay Krykunov <nik.krikunov.99@gmail.com>
2021-06-06 18:13:49 +0300

    add animals slider & footer

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,10 +1,39 @@
+import { tns } from 'tiny-slider/src/tiny-slider';
+
 const body = document.querySelector('body');
 const header_burger = document.querySelector('.header__burger');
 const header_menu = document.querySelector('.header__menu');
-
+const pets_sliders = document.querySelectorAll('.adoptions_list');
 
 header_burger.addEventListener('click', function(event){
     header_burger.classList.toggle('active');
     header_menu.classList.toggle('active');
     body.classList.toggle('no-scroll');
+});
+
+
+pets_sliders.forEach(item => {
+    const container = item.querySelector('.slider__container');
+    const prev_button = item.querySelector('.slider__prev');
+    const next_button = item.querySelector('.slider__next');
+
+    tns({
+        container: container,
+        autoplayButton:false,
+        autoplayTimeout:3000,
+        nav:false,
+        prevButton: prev_button,
+        nextButton: next_button,
+        autoWidth: false,
+        loop:true,
+        mouseDrag:true,
+        responsive:{
+            640:{
+                items: 1
+            },
+            800: {
+                items: 3
+            }
+        }
+    });
 });
